e9f42a4fa6a863796d89944e916c57f2233955dd
Kowsihan <kowsihan2sk@gmail.com>
2020-06-21 09:42:53 +0530

    Football

---
+++ CF-A/CF43-D2-A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s[100];
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    sort(s, s + n);
+    cout << s[n / 2];
+    return 0;
+}
